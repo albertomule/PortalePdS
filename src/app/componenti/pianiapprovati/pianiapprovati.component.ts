53e524807743a9bb6ef8e5046738d84156a8f444
albertomule <alberto.ml@tiscali.it>
2024-08-28 08:06:04 +0200

    Pianiapprovati view

--- src/app/componenti/pianiapprovati/pianiapprovati.component.ts
+++ src/app/componenti/pianiapprovati/pianiapprovati.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pianiapprovati',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './pianiapprovati.component.css'
 })
 export class PianiapprovatiComponent {
+  
+  form: FormGroup = this.fb.group({
+    matricola: ['', [Validators.required, Validators.pattern('^[0-9]{6}$')]]
+  })
 
+  constructor(private fb: FormBuilder) {}
+  
+  get matricola() { return this.form.get("matricola") }
+
+  getMatricola(){
+    return this.form.get("matricola")!.value
+  }
+
+  submit(form: FormGroupDirective) {
+    if (form.valid) {
+      console.log(JSON.stringify(this.form.value));
+      // include code here to send form data to backend
+    }
+  }
 }
--- src/app/componenti/piano/piano.component.ts
+++ src/app/componenti/piano/piano.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { MongoService } from '../../servizi/mongo.service';
 
 @Component({
   selector: 'app-piano',
   templateUrl: './piano.component.html',
   styleUrl: './piano.component.css'
 })
-export class PianoComponent {
+export class PianoComponent implements OnInit, OnDestroy{
 
+  matricola: string = ""
+  a: boolean = false
+
+  sottoscrizione: any
+  //submongo: any
+
+  constructor(private route: ActivatedRoute, private mongo: MongoService){}
+
+  ngOnInit(): void {
+    this.sottoscrizione = this.route.paramMap.subscribe((params: ParamMap)=>{
+      this.matricola = params.get('id')!
+      if(this.matricola.includes("a")){
+        this.matricola = this.matricola.replace("a","")
+        this.a = true
+      }
+      //  this.submongo = this.mongo.getRegolamento(this.anno).subscribe((data: any) => {
+      //   this.regolamento = Object.values(data)
+        
+      //   this.getPrimo()
+      //   this.getSecondo()
+      //   this.getTerzo()
+      //   this.getComp()
+        
+      //   console.log(this.regolamento)
+      //   console.log(this.primo)
+      //   console.log(this.secondo)
+      //   console.log(this.terzo)
+      //   console.log(this.comp)
+      // })
+    })
+    console.log(this.matricola)
+  }
+
+  ngOnDestroy(): void {
+    this.sottoscrizione.unsubscribe()
+    //this.submongo.unsubscribe()
+  }
+
+  getA() { return this.a }
 }
